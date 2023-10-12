6910be24a7117cec1a4748f9b2e0d27311088b65
NILESH SHARMA <91261508+nilesh45678@users.noreply.github.com>
2023-10-12 23:51:45 +0530

    Add files via upload

---
+++ Exp_no_01_temp_humidity_on_serialMonitor.ino
@@ -0,0 +1,29 @@
+#include <DHT.h>  //DHT library
+
+#define DHTPIN 2      // Digital pin connected to the DHT sensor  
+#define DHTTYPE DHT11     
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup(){ 
+  Serial.begin(9600);  // Initialize Serial monitor at 9600 baud rate
+  Serial.println(F("DHT11 test!"));
+  dht.begin();    // Initialize DHT sensor.
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+  delay(2000);
+}
